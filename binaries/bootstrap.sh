c0d333844cd02b75d8dd6c227faadfa344014dc6
mohamed.abukar <mo-abukar10@hotmail.com>
2022-04-09 18:48:52 +0100

    fix shell script

--- binaries/bootstrap.sh
+++ binaries/bootstrap.sh
@@ -9,4 +9,4 @@ python3 get-pip.py
 sudo pip3 install --ignore-installed ansible
 ansible-galaxy install -r requirements.yml
 
-ansible-playbook -i "localhost," -c local ansible_osx.yml --ask-become-pass
+ansible-playbook -i "localhost," -c local ansible_mac.yml --ask-become-pass
